aec1c253ae2dfbabf8918c002006785d0d505a66
Juntia <114236242+AmauryDufour@users.noreply.github.com>
2024-12-04 17:41:55 +0800

    improve bbox handling

--- tomtom.py
+++ tomtom.py
@@ -1,9 +1,5 @@
 # Python: Fetch TomTom Traffic Incidents and store in SQLite
 
-import requests
-from datetime import datetime
-import schedule
-import time
 import os
 import json
 from dotenv import load_dotenv
@@ -13,12 +9,13 @@ from utils import Geocode, TrafficIncidents
 load_dotenv()
 API_KEY = os.getenv('TOMTOM_API_KEY')
 BASE_URL = os.getenv('BASE_URL')
-TRAFFIC_INCIDENTS_API_URL = json.loads(os.getenv('TRAFFIC_INCIDENTS_API_URL'))
-GEOCODING_API_URL = json.loads(os.getenv('GEOCODING_API_URL'))
+
+TRAFFIC_INCIDENTS_API_URLS = json.loads(os.getenv('TRAFFIC_INCIDENTS_API_URLS'))
+GEOCODING_API_URLS = json.loads(os.getenv('GEOCODING_API_URLS'))
 
 INCIDENTS_params = {
     'key': API_KEY,
-    'bbox': '1.363763, 103.712294, 1.334160, 103.754866', # EPSG:4326 - WGS 84 projection
+    'bbox': '', # EPSG:4326 - WGS 84 projection "min_lon,min_lat,max_lon,max_lat"
     'fields' : '{incidents{type,geometry{type,coordinates},properties{id,iconCategory,magnitudeOfDelay,events{description,code,iconCategory},startTime,endTime,from,to,length,delay,roadNumbers,timeValidity,probabilityOfOccurrence,numberOfReports,lastReportTime,tmc{countryCode,tableNumber,tableVersion,direction,points{location,offset}}}}}',
     'language': 'en-GB',
     'timeValidityFilter': 'present'
@@ -31,8 +28,8 @@ GEOCODING_params = {
 if __name__ == "__main__":
     location = "Singapore"
 
-    Geocode_API = Geocode(GEOCODING_API_URL)
-    IncidentsAPI = TrafficIncidents(TRAFFIC_INCIDENTS_API_URL)
+    Geocode_API = Geocode(GEOCODING_API_URLS)
+    IncidentsAPI = TrafficIncidents(TRAFFIC_INCIDENTS_API_URLS)
     
     INCIDENTS_params['bbox'] = Geocode_API.reformatbbox(Geocode_API.get_bbox(GEOCODING_params, location))
     IncidentsAPI.get_incidents(INCIDENTS_params)
